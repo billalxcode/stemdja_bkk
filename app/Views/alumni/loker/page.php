fe346012753c204900529000524c5f3dd936085d
billalxcode <billal.xcode@gmail.com>
2022-12-30 15:29:05 +0700

    New commit

--- app/Views/alumni/loker/page.php
+++ app/Views/alumni/loker/page.php
@@ -24,52 +24,58 @@ use CodeIgniter\I18n\Time;
         </div>
         <h5 class="order-2">List Loker:</h5>
         <div class="col-lg-12 mb-4 order-3">
-            <div class="row">
-                <?php foreach ($lokerData as $loker): ?>
-                    <div class="col-lg-6 col-12 col-sm-12 col-md-6 my-2">
-                        <div class="card">
-                            <div class="card-header">
-                                <a href="<?= base_url('alumni/loker/' . $loker['id']) ?>" class="card-title">
-                                    <?= $loker['title'] ?>
-                                    <small class="float-end text-dark">
-                                        <?php
-                                        $time = new Time($loker['created_at']);
-                                        echo $time->humanize();
-                                        ?>
-                                    </small>
-                                </a>
-                            </div>
-                            <div class="card-body">
-                                <?php
-                                $word = explode(' ', $loker['kualifikasi']);
-                                $str = '';
-                                if(count($word) >= 20) {
-                                    for ($i = 0; $i < 20; $i++) {
-                                        $str .=  $word[$i] . ' ';
-                                    }
-                                    $str .= '...';
-                                } else {
-                                    $str = $loker['kualifikasi'];
-                                }
-
-                                echo $str;
-                                ?>
-                            </div>
-                            <div class="card-footer">
-                                <div class="float-start">
-                                    <b><?= $loker['corporate_name'] ?></b>
+            <?php if (count($lokerData) == 0): ?>
+                <div class="alert alert-danger">
+                    Mohon maaf saat ini loker <?= (isset($search) ? "<span class='fw-bold'>'" . $search . "'</span>" : '') ?> tidak tersedia
+                </div>
+            <?php else: ?>
+                <div class="row">
+                    <?php foreach ($lokerData as $loker): ?>
+                        <div class="col-lg-6 col-12 col-sm-12 col-md-6 my-2">
+                            <div class="card">
+                                <div class="card-header">
+                                    <a href="<?= base_url('alumni/loker/' . $loker['id']) ?>" class="card-title">
+                                        <?= $loker['title'] ?>
+                                        <small class="float-end text-dark">
+                                            <?php
+                                            $time = new Time($loker['created_at']);
+                                            echo $time->humanize();
+                                            ?>
+                                        </small>
+                                    </a>
                                 </div>
-                                <a href="<?= base_url('/alumni/loker/' . $loker['id']) ?>" class="btn btn-secondary btn-sm float-end mx-1">
-                                    Lihat
-                                </a>
-                                <a href="https://wa.me/<?= $loker['corporate_contact'] ?>" class="btn btn-danger btn-sm float-end mx-1">
-                                    Lamar
-                                </a>
+                                <div class="card-body">
+                                    <?php
+                                    $word = explode(' ', $loker['kualifikasi']);
+                                    $str = '';
+                                    if(count($word) >= 20) {
+                                        for ($i = 0; $i < 20; $i++) {
+                                            $str .=  $word[$i] . ' ';
+                                        }
+                                        $str .= '...';
+                                    } else {
+                                        $str = $loker['kualifikasi'];
+                                    }
+
+                                    echo $str;
+                                    ?>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="float-start">
+                                        <b><?= $loker['corporate_name'] ?></b>
+                                    </div>
+                                    <a href="<?= base_url('/alumni/loker/' . $loker['id']) ?>" class="btn btn-secondary btn-sm float-end mx-1">
+                                        Lihat
+                                    </a>
+                                    <a href="https://wa.me/<?= $loker['corporate_contact'] ?>" class="btn btn-danger btn-sm float-end mx-1">
+                                        Lamar
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach ?>
-            </div>
+                    <?php endforeach ?>
+                </div>
+            <?php endif ?>
         </div>
     </div>
 </div>
